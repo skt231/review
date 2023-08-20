ccf6bea803a90cb7109442ff27a03ad11d210fa9
skt231 <shimguta123@gmail.com>
2023-08-20 21:40:58 +0900

    08-20

--- js_css/JS/DOM/Manipurator/app.js
+++ js_css/JS/DOM/Manipurator/app.js
@@ -42,30 +42,81 @@ for (let link of allLinks) {
     link.style.textDecorationStyle = 'wavy'
 }
 // attribute...//getAttribute()
-const firstLink=document.querySelector('a');
+const firstLink = document.querySelector('a');
 firstLink.getAttribute('href')
 // setAttirbute()
-firstLink.setAttribute('href','http://www.google.com');
+firstLink.setAttribute('href', 'http://www.google.com');
 
 
 //change style
 // h1.style.color='green'
 //h1.style.border='2px solid pink'
-const AllLink=document.querySelector('a');
+const AllLink = document.querySelector('a');
 // for(let link of allLinks){
 //     link.style.color='rgb(0,108,134)';
-        // link.style.textDecorationColor='magenta';
-        // link.style.textDecoration='wavy';
+// link.style.textDecorationColor='magenta';
+// link.style.textDecoration='wavy';
 // }
 // window.getComputedStyle(h1).fontSize    ....style을 가저욤
 AllLink.style.borderRadius
 
 // classList
-const h2=document.querySelector('h2')
+const h2 = document.querySelector('h2')
 h2.classList.add('purple')
 h2.classList.add('border')
 // h2.classList.remove('border')
 // h2.classList.contains('border')//true /
 // h2.classList.toggle('pa')//false//클래스가 이미 존재하면 제거하고, 존재하지 않으면 추가
+
+// //parentElemnet ..
+const firBold = document.querySelector('b');
+// firstBold //<b>Silkie</b>
+// firstBold.parentElement // <body></body>
+// firstBold.parentElement.parentElement//<html lang="en"> <head></head><body></body></html>
+const paragraph = firBold.parentElement;
+// console.log(paragraph.children)//[b, b, a, a, a, a, a, a]
+const squareImg = document.querySelector('.square');
+// const parg=squareImg.parentElement;
+console.log(squareImg.children)
+//nextSibling.... //#text
+//nextElementSibling.... .. // <img class='square' src="" alt>
+//previousElementSibling    //<p></p>
+//위의 3개 찾아보자
+
+//Append&AppendChild
+//appendChild는 DOM 노드에 새로운 자식 노드를 추가하는 메서드입니다. 이 메서드를 사용하면 특정 부모 노드에 새로운 자식 노드를 추가할 수 있습니다.
+//append:DOM 요소에 새로운 자식 노드를 추가하는 메서드.HTML 요소 내에 다른 HTML 요소나 텍스트를 추가할 수 있습니다.
+// const parentElement = document.getElementById('parent'); // 부모 요소 선택
+// const childElement = document.createElement('div'); // 새로운 자식 요소 생성
+// childElement.textContent = 'Hello, World!'; // 자식 요소의 내용 설정
+// parentElement.append(childElement); // 부모 요소에 자식 요소 추가
+const newImg = document.createElement('img');
+console.dir(newImg)//img
+newImg.src = 'https://t1.daumcdn.net/cfile/tistory/2238484058DB98CB29';
+document.body.appendChild(newImg)
+newImg.classList.add('square')
+const newH3=document.createElement('h3')
+newH3.innerText='I am new!';
+document.body.appendChild(newH3)
+
+const p=document.querySelector('p')
+//prepend
+p.append('||||||||i am new textyqyqyq|||||||||||')//맨 뒤에 추가됨
+const newB=document.createElement('b');
+//append
+newB.append('||Hi!||')
+p.prepend(newB)//맨앞에 추가됨
+
+//인접요소의 경우에는  beforebegin이나 afterend를 쓴다.
+const h2_1=document.createElement('h2');
+h2_1.append("Are adorable chickens");
+const h1=document.querySelector('h1')
+h1.insertAdjacentElement('beforebegin',h2_1)
+const h3=document.createElement('h3')
+h3.innerText='I am h3'; 
+h1.after(h3)
+
+
+
 
 
